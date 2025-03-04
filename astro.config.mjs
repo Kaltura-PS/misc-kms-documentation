bde9082f43431b0ee2a8a93bb68578a0dce527c8
Muli Dayan <1042806+muli@users.noreply.github.com>
2025-03-04 11:20:06 +0200

    PSVAMB-70091 configure gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://kaltura-ps.github.io",
+    base: "/misc-kms-documentation",
     integrations: [
         starlight({
             title: "MediaSpace Dev Documentation",
